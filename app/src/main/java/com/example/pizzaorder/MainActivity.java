ab690d920f9a83d99acdd4f9ed6e2c08930d44b0
Trinh Trung <you@example.com>
2020-02-07 01:03:26 -0500

    first commit

---
+++ app/src/main/java/com/example/pizzaorder/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pizzaorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static Button button1 ;
+
+    public void onClickButtonListener(){
+        button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // args for constructor take the action of 2nd activity in the manifest file
+                Intent intent = new Intent("android.intent.action.MenuActivity");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onClickButtonListener();
+    }
+}
